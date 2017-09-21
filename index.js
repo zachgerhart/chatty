5febf0151b0ed2515aae5e8ec18af28318f7321e
zachgerhart <zgerhart@gmail.com>
2017-09-21 09:20:56 -0500

    message

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const port = 3000;
+
+const messages = [ "yooo"
+
+]
+
+
+var app = express();
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
+})
